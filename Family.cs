66f67d2889d41e8d7b7338498bd070e8e3699c14
YulianNedelchev <66035732+YulianNedelchev@users.noreply.github.com>
2020-05-27 22:56:34 +0300

    Add files via upload
    test

---
+++ Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    class Family
+    {
+        public Family()
+        {
+            this.FamilyMembers=new List<Person>();
+        }
+        public List<Person> FamilyMembers {get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.FamilyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.FamilyMembers
+                .OrderByDescending(x => x.Age)
+                .First();
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var family = new Family();
+            for (int i = 0; i < lines; i++)
+            {
+                var currentLine = Console.ReadLine().Split();
+                var currentPerson = new Person(currentLine[0], int.Parse(currentLine[1]));
+                family.AddMember(currentPerson);
+            }
+
+            var oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
